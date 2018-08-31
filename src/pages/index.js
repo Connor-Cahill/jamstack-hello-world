76f0bf46568f710be986cc32afa0ea1957c49c3a
Connor Cahill <connor@Connors-MacBook.local>
2018-08-30 22:58:29 -0700

    git init

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello Everyone</h1>
+    <p>My name is Connor and I am currently a full stack web student at Make School in SF.</p>
+    <p>I am excited to build something cool at the JAMstack Hackathon</p>
+  </div>
+)
+
+export default IndexPage
